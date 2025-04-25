b5fa1eb82beda7700167beb724d6023981ffa0f6
Xuho <ngotranxuanhoa09062004@gmail.com>
2025-04-25 21:09:23 +0700

    fix default theme and language

--- fusionnews-ui/src/helpers/language/Languagelayout.tsx
+++ fusionnews-ui/src/helpers/language/Languagelayout.tsx
@@ -13,9 +13,13 @@ const LanguageToggle: React.FC = () => {
   };
 
   return (
-    <button onClick={handleToggle}>
-      {language === 'en' ? '🇬🇧 English' : '🇻🇳 Tiếng Việt'}
-    </button>
+    <button
+    onClick={handleToggle}
+    className="bg-gray-100 shadow-lg rounded-2xl px-4 py-2 text-sm font-medium hover:bg-gray-200 transition duration-300"
+  >
+    {language === 'en' ? '🇬🇧 English' : '🇻🇳 Tiếng Việt'}
+  </button>
+  
   );
 };
 
