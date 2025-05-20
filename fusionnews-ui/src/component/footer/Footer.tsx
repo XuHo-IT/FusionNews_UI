5485ddf140958ad86b7653d3268a554643e03dc4
Xuho <ngotranxuanhoa09062004@gmail.com>
2025-05-20 11:30:06 +0700

    fix UI and language in all page

--- fusionnews-ui/src/component/footer/Footer.tsx
+++ fusionnews-ui/src/component/footer/Footer.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import './footer.css'
 import { NavLink } from 'react-router-dom'
+import { useSelector } from "react-redux";
+import { RootState } from "redux/store";
+import { translations } from "helpers/languageMap";
 
 const Footer = () => {
+      const language = useSelector((state: RootState) => state.language.language);
   return (
     <div>
       <div className="container-fluid bg-dark pt-5 px-sm-3 px-md-5 mt-5">
         <div className="row py-4">
-            <div className="col-lg-3 col-md-6 mb-5">
-                <h5 className="mb-4 text-white text-uppercase font-weight-bold">Get In Touch</h5>
-                <p className="font-weight-medium"><i className="fa fa-map-marker-alt mr-2"></i>123 Street, New York, USA</p>
-                <p className="font-weight-medium"><i className="fa fa-phone-alt mr-2"></i>+012 345 67890</p>
-                <p className="font-weight-medium"><i className="fa fa-envelope mr-2"></i>info@example.com</p>
-                <h6 className="mt-4 mb-3 text-white text-uppercase font-weight-bold">Follow Us</h6>
-                <div className="d-flex justify-content-start">
+            {/* <div className="col-lg-3 col-md-6 mb-5"> */}
+                <h5 className="mb-4 text-white text-uppercase font-weight-bold">{translations[language as "en" | "vi"].contact}</h5>
+                <p className="font-weight-medium text-white"><i className="fa fa-map-marker-alt mr-2"></i>  {translations[language as "en" | "vi"].contact_info}</p>
+                <p className="font-weight-medium text-white"><i className="fa fa-envelope mr-2"></i>  {translations[language as "en" | "vi"].contact_email}</p>
+                <h6 className="mt-4 mb-3 text-white text-uppercase font-weight-bold"> {translations[language as "en" | "vi"].followUs}</h6>
+                <div className="d-flex justify-content-center">
                     <NavLink className="btn btn-lg btn-secondary btn-lg-square mr-2" to="/"><i className="fab fa-twitter"></i></NavLink>
                     <NavLink className="btn btn-lg btn-secondary btn-lg-square mr-2" to="/"><i className="fab fa-facebook-f"></i></NavLink>
                     <NavLink className="btn btn-lg btn-secondary btn-lg-square mr-2" to="/"><i className="fab fa-linkedin-in"></i></NavLink>
                     <NavLink className="btn btn-lg btn-secondary btn-lg-square mr-2" to="/"><i className="fab fa-instagram"></i></NavLink>
                     <NavLink className="btn btn-lg btn-secondary btn-lg-square" to="/"><i className="fab fa-youtube"></i></NavLink>
                 </div>
-            </div>
-            <div className="col-lg-3 col-md-6 mb-5">
+            {/* </div> */}
+            {/* <div className="col-lg-3 col-md-6 mb-5">
                 <h5 className="mb-4 text-white text-uppercase font-weight-bold">Popular News</h5>
                 <div className="mb-3">
                     <div className="mb-2">
@@ -44,8 +47,8 @@ const Footer = () => {
                     </div>
                     <NavLink className="small text-body text-uppercase font-weight-medium" to="/">Lorem ipsum dolor sit amet elit. Proin vitae porta diam...</NavLink>
                 </div>
-            </div>
-            <div className="col-lg-3 col-md-6 mb-5">
+            </div> */}
+            {/* <div className="col-lg-3 col-md-6 mb-5">
                 <h5 className="mb-4 text-white text-uppercase font-weight-bold">Categories</h5>
                 <div className="m-n1">
                     <NavLink to="/" className="btn btn-sm btn-secondary m-1">Politics</NavLink>
@@ -70,8 +73,8 @@ const Footer = () => {
                     <NavLink to="/" className="btn btn-sm btn-secondary m-1">Business</NavLink>
                     <NavLink to="/" className="btn btn-sm btn-secondary m-1">Foods</NavLink>
                 </div>
-            </div>
-            <div className="col-lg-3 col-md-6 mb-5">
+            </div> */}
+            {/* <div className="col-lg-3 col-md-6 mb-5">
                 <h5 className="mb-4 text-white text-uppercase font-weight-bold">Flickr Photos</h5>
                 <div className="row">
                     <div className="col-4 mb-3">
@@ -93,14 +96,12 @@ const Footer = () => {
                         <NavLink to="/"><img className="w-100" src="img/news-110x110-1.jpg" alt=""/></NavLink>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     </div>
     <div className="container-fluid py-4 px-sm-3 px-md-5" style={{background:" #111111"}}>
-        <p className="m-0 text-center">&copy; <NavLink to="/">Your Site Name</NavLink>. All Rights Reserved. 
-		
-
-		Design by <NavLink to="https://htmlcodex.com">HTML Codex</NavLink></p>
+        <p className="m-0 text-center text-white">&copy; <NavLink to="/">FusionNews</NavLink>.  {translations[language as "en" | "vi"].rights}. 
+		{translations[language as "en" | "vi"].design} <NavLink to="https://github.com/XuHo-IT/FusionNews_UI">{translations[language as "en" | "vi"].team}</NavLink></p>
     </div>
    
     </div>
